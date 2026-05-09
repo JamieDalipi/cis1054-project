0d44b5bc66b2fbde3c88dbaec9fd465faec3b768
JamieDalipi <jdal0024@um.edu.mt>
2026-05-09 23:26:23 +0200

    Create Desserts.php

---
+++ Desserts.php
@@ -0,0 +1,14 @@
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once 'vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__);
+$twig = new Environment($loader);
+
+echo $twig->render('Desserts.twig', [
+    'siteName' => 'CampusEats',
+    'currentPage' => 'Desserts.php'
+]);
