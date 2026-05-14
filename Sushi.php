4e8d605604b31966d0ac0af644a3e140247ed7ff
JamieDalipi <jdal0024@um.edu.mt>
2026-05-14 18:12:39 +0200

    Load sushi items from JSON and pass to Twig template

--- Sushi.php
+++ Sushi.php
@@ -7,8 +7,26 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
+$jsonFile = __DIR__ . '/Database.json';
+$jsonRaw = file_get_contents($jsonFile);
+$jsonData = json_decode($jsonRaw, true);
+$sushiItems = [];
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Sushi') {
 
+        $sushiItems[] = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+            'desc' => $item['desc'],
+            'Ingredients' => $item['Ingredients'],
+            'Category' => $item['Category'],
+            'price' => $item['Price']
+        ];
+    }
+}
 echo $twig->render('Sushi.twig', [
     'siteName' => 'CampusEats',
-    'currentPage' => 'Sushi.php'
+    'currentPage' => 'Sushi.php',
+    'sushi' => $sushiItems
 ]);
