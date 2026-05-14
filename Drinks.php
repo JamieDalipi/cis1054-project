ed33d25a994b7a2febc18981c2306b1788642d92
JamieDalipi <jdal0024@um.edu.mt>
2026-05-14 18:04:33 +0200

    Load drinks data from JSON and pass to Twig template

--- Drinks.php
+++ Drinks.php
@@ -7,8 +7,26 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
+$jsonFile = __DIR__ . '/Database.json';
+$jsonRaw = file_get_contents($jsonFile);
+$jsonData = json_decode($jsonRaw, true);
+$DrinksItems = [];
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Drinks') {
 
+        $DrinksItems[] = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+            'desc' => $item['desc'],
+            'Ingredients' => $item['Ingredients'],
+            'Category' => $item['Category'],
+            'price' => $item['Price']
+        ];
+    }
+}
 echo $twig->render('Drinks.twig', [
     'siteName' => 'CampusEats',
-    'currentPage' => 'Drinks.php'
+    'currentPage' => 'Drinks.php',
+    'drinks' => $DrinksItems
 ]);
