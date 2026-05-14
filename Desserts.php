395742c2dab63708db248e86ae15b9b1098fa14a
JamieDalipi <jdal0024@um.edu.mt>
2026-05-14 17:39:50 +0200

    Load dessert items from JSON and render in template
    Added functionality to load dessert items from a JSON file and pass them to the Twig template.

--- Desserts.php
+++ Desserts.php
@@ -7,8 +7,26 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
+$jsonFile = __DIR__ . '/Database.json';
+$jsonRaw = file_get_contents($jsonFile);
+$jsonData = json_decode($jsonRaw, true);
+$dessertItems = [];
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Desserts') {
 
+        $dessertItems[] = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+            'desc' => $item['desc'],
+            'Ingredients' => $item['Ingredients'],
+            'Category' => $item['Category'],
+            'price' => $item['Price']
+        ];
+    }
+}
 echo $twig->render('Desserts.twig', [
     'siteName' => 'CampusEats',
-    'currentPage' => 'Desserts.php'
+    'currentPage' => 'Desserts.php',
+    'dessertItems' => $dessertItems
 ]);
