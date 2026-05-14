83babb12a7e6a46f9d467d6cd979ea3b9ba9524d
JamieDalipi <jdal0024@um.edu.mt>
2026-05-14 18:05:18 +0200

    Load and filter main items from JSON data

--- Main.php
+++ Main.php
@@ -7,8 +7,26 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
+$jsonFile = __DIR__ . '/Database.json';
+$jsonRaw = file_get_contents($jsonFile);
+$jsonData = json_decode($jsonRaw, true);
+$mainItems = [];
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Main') {
 
+        $mainItems[] = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+            'desc' => $item['desc'],
+            'Ingredients' => $item['Ingredients'],
+            'Category' => $item['Category'],
+            'price' => $item['Price']
+        ];
+    }
+}
 echo $twig->render('Main.twig', [
     'siteName' => 'CampusEats',
-    'currentPage' => 'Main.php'
+    'currentPage' => 'Main.php',
+    'main' => $mainItems
 ]);
