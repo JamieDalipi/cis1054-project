913dfa1c0dc6e7b156e53e058337b805556e289d
JamieDalipi <jdal0024@um.edu.mt>
2026-05-14 18:02:41 +0200

    Load breakfast items from JSON for Twig rendering
    Added functionality to load breakfast items from a JSON file and pass them to the Twig template.

--- Breakfast.php
+++ Breakfast.php
@@ -7,8 +7,26 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
+$jsonFile = __DIR__ . '/Database.json';
+$jsonRaw = file_get_contents($jsonFile);
+$jsonData = json_decode($jsonRaw, true);
+$breakfastItems = [];
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Breakfast') {
 
+        $breakfastItems[] = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+            'desc' => $item['desc'],
+            'Ingredients' => $item['Ingredients'],
+            'Category' => $item['Category'],
+            'price' => $item['Price']
+        ];
+    }
+}
 echo $twig->render('Breakfast.twig', [
     'siteName' => 'CampusEats',
-    'currentPage' => 'Breakfast.php'
+    'currentPage' => 'Breakfast.php',
+    'breakfast' => $breakfastItems
 ]);
