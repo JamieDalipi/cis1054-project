dc0b95d1180a9ddc3584dcc64e2a8ffc489519ea
JamieDalipi <jdal0024@um.edu.mt>
2026-05-14 18:08:42 +0200

    Extract menu items from JSON by category

--- Menu.php
+++ Menu.php
@@ -7,8 +7,75 @@ require_once 'vendor/autoload.php';
 
 $loader = new FilesystemLoader(__DIR__);
 $twig = new Environment($loader);
+$jsonFile = __DIR__ . '/Database.json';
+$jsonRaw = file_get_contents($jsonFile);
+$jsonData = json_decode($jsonRaw, true);
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Breakfast') {
 
+        $foundItem = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+        ];
+
+        break;
+    }
+}
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Sushi') {
+
+        $foundItem1 = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+        ];
+
+        break;
+    }
+}
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Main') {
+
+        $foundItem2 = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+        ];
+
+        break;
+    }
+}
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Desserts') {
+
+        $foundItem3 = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+        ];
+
+        break;
+    }
+}
+foreach ($jsonData['Data'] as $item) {
+    if ($item['Category'] == 'Drinks') {
+
+        $foundItem4 = [
+            'id' => $item['ID'],
+            'name' => $item['Name'],
+            'image' => $item['img'],
+        ];
+
+        break;
+    }
+}
 echo $twig->render('Menu.twig', [
     'siteName' => 'CampusEats',
-    'currentPage' => 'Menu.php'
+    'currentPage' => 'Menu.php',
+    'breakfastItem' => $foundItem,
+    'sushiItem' => $foundItem1,
+    'mainItem' => $foundItem2,
+    'dessertItem' => $foundItem3,
+    'DrinksItem' => $foundItem4
 ]);
