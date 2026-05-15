df52f832a343418f798485022508d1c497a4c0da
JamieDalipi <jdal0024@um.edu.mt>
2026-05-15 19:58:31 +0200

    Refactor contact form handling with Twig integration

--- contact.php
+++ contact.php
@@ -1,45 +1,73 @@
-<html>
-<body>
+<?php
 
-Your message has been received <?php echo $_POST["name"]; ?><br>
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-$name = $email = $message ="";
+require_once __DIR__.'/bootstrap.php';
+require_once 'vendor/autoload.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $message = tes_input($_POST["email"]);
-}
+$loader = new FilesystemLoader(__DIR__);
+$twig = new Environment($loader);
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+$json = file_get_contents(__DIR__.'/database.json');
+$data = json_decode($json, true);
 
-$nameErr = $emailErr = "";
+session_start();
+
+$nameErr = $emailErr = $messageErr = "";
+
 $name = $email = $message = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
-
-  if (empty($_POST["message"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["message"]);
-  }
+function test_input($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
 }
 
-</body>
-</html>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $valid = true;
+
+    if (empty($_POST["fname"])) {
+        $nameErr = "Name is required";
+        $valid = false;
+    } else {
+        $name = test_input($_POST["fname"]);
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+        $valid = false;
+    } else {
+        $email = test_input($_POST["email"]);
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+            $valid = false;
+        }
+    }
+
+    if (empty($_POST["comment"])) {
+        $messageErr = "Message is required";
+        $valid = false;
+    } else {
+        $message = test_input($_POST["comment"]);
+    }
+
+    if ($valid) {
+
+        $_SESSION["form_data"] = [
+            "name" => $name,
+            "email" => $email,
+            "message" => $message
+        ];
+
+        header("Location: Redirect.php");
+        exit();
+    }
+}
+
+echo $twig->render('Contact-us.twig', [
+    'business' => $data['business'][0],
+    'nameErr' => $nameErr,
+    'emailErr' => $emailErr,
+    'messageErr' => $messageErr
+]);
